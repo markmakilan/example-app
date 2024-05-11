3faf54bada3968fe3b08f87800d3fdaa0fd0b9f6
Mark Makilan <mark.makilan@gets.ph>
2024-05-11 14:12:08 +0800

    latest

--- resources/views/livewire/pages/admin/layouts/topbar.blade.php
+++ resources/views/livewire/pages/admin/layouts/topbar.blade.php
@@ -1,3 +1,8 @@
-<div class="flex items-center h-8 px-4">
-    <button class="w-4 h-4 bg-secondary rounded-sm hover:bg-accent" x-show="!sidebar" x-on:click="toggleSidebar"></button>
+<div class="flex items-center justify-between h-8 px-4">
+    <div class="flex items-center gap-1">
+        <button class="w-4 h-4 bg-secondary rounded-sm hover:bg-accent" x-show="!sidebar" x-on:click="toggleSidebar"></button>
+    </div>
+    <div class="flex items-center gap-4">
+        <button class="w-4 h-4 bg-secondary rounded-sm hover:bg-accent"></button>
+    </div>
 </div>
