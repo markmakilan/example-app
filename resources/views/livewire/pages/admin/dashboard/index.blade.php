b0c346d4e4eecb45c7b9fca307ba9c794a62a620
Mark Makilan <mark.makilan@gets.ph>
2024-05-11 14:17:13 +0800

    remove content

--- resources/views/livewire/pages/admin/dashboard/index.blade.php
+++ resources/views/livewire/pages/admin/dashboard/index.blade.php
@@ -2,36 +2,5 @@
     <div class="min-w-full h-12 flex items-center px-4">
         <h1 class="text-lg font-semibold">Dashboard</h1>
     </div>
-    <div class="flex-1 px-4 py-2 overflow-y-auto scrollable">
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-        <p>Care about people's approval and you will be their prisoner.</p>
-    </div>
+    <div class="flex-1 px-4 py-2 overflow-y-auto scrollable"></div>
 </div>
