a5d226d2dd64f12e452c044b9f075d56a5e621e1
Mark Makilan <mark.makilan@gets.ph>
2024-05-11 14:41:34 +0800

    ui enhancements

--- resources/views/livewire/pages/admin/layouts/sidebar.blade.php
+++ resources/views/livewire/pages/admin/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="w-64 h-full group/toggle-button" x-show="sidebar">
     <div class="flex flex-col h-full divide-y">
         <div class="h-12 flex items-center relative">
-            <button class="absolute -right-3 rounded-sm p-1 hover:bg-secondary group-hover/toggle-button:block" x-on:click="toggleSidebar">
-                <div class="w-4 h-4 bg-accent"></div>
+            <button class="absolute group/sidebar-toggle -right-3 rounded-sm p-1 hover:bg-secondary group-hover/toggle-button:block" x-on:click="toggleSidebar">
+                <div class="w-4 h-4 bg-secondary group-hover/sidebar-toggle:bg-accent"></div>
             </button>
         </div>
         <div class="flex-1 overflow-y-auto scrollable">
@@ -11,7 +11,7 @@
                     <li class="px-4">
                         <a href="#">
                             <div class="flex items-center gap-1.5 rounded-md px-3 py-1.5 hover:bg-zinc-100">
-                                <span class="w-4 h-4 rounded-sm bg-accent"></span>
+                                <span class="w-4 h-4 rounded-sm bg-secondary"></span>
                                 <span>Menu</span>
                             </div>
                         </a>
@@ -20,7 +20,7 @@
                         <div class="flex items-center justify-between rounded-md cursor-pointer px-3 py-1.5 hover:bg-zinc-100 group/dropdown-menu-1" x-on:click="toggleDropdownMenu">
                             <div class="flex items-center gap-1.5">
                                 <span
-                                    class="w-4 h-4 rounded-sm bg-accent"
+                                    class="w-4 h-4 rounded-sm bg-secondary"
                                 ></span>
                                 <span>Dropdown Menu 1</span>
                             </div>
@@ -46,24 +46,24 @@
                         </ul>
                     </li>
                     <li class="space-y-1 px-4" x-data="{ dropdown: false, toggleDropdownMenu() { this.dropdown = !this.dropdown } }">
-                        <div class="flex items-center justify-between rounded-md cursor-pointer px-3 py-1.5 hover:bg-zinc-100 group/dropdown-menu-2" x-on:click="toggleDropdownMenu">
+                        <div class="flex items-center justify-between rounded-md cursor-pointer px-3 py-1.5 hover:bg-zinc-100 group/dropdown-menu-1" x-on:click="toggleDropdownMenu">
                             <div class="flex items-center gap-1.5">
                                 <span
-                                    class="w-4 h-4 rounded-sm bg-accent"
+                                    class="w-4 h-4 rounded-sm bg-secondary"
                                 ></span>
                                 <span>Dropdown Menu 2</span>
                             </div>
-                            <span class="w-4 h-4 rounded-sm bg-secondary hidden group-hover/dropdown-menu-2:block"></span>
+                            <span class="w-4 h-4 rounded-sm bg-secondary hidden group-hover/dropdown-menu-1:block"></span>
                         </div>
                         <ul class="space-y-1" x-show="dropdown" x-collapse>
                             <li x-data="{ dropdown: false, toggleDropdownMenu() { this.dropdown = !this.dropdown } }">
-                                <div class="flex items-center justify-between rounded-md cursor-pointer px-3 py-1.5 hover:bg-zinc-100 group/dropdown-menu-3" x-on:click="toggleDropdownMenu">
+                                <div class="flex items-center justify-between rounded-md cursor-pointer px-3 py-1.5 hover:bg-zinc-100 group/dropdown-menu-2" x-on:click="toggleDropdownMenu">
                                     <div class="flex items-center gap-1.5">
                                         <span class="w-4 h-4 invisible"></span>
                                         <span>Dropdown Sub Menu</span>
                                     </div>
                                     
-                                    <span class="w-4 h-4 rounded-sm bg-secondary hidden group-hover/dropdown-menu-3:block"></span>
+                                    <span class="w-4 h-4 rounded-sm bg-secondary hidden group-hover/dropdown-menu-2:block"></span>
                                 </div>
                                 <ul class="space-y-1" x-show="dropdown" x-collapse>
                                     <li>
@@ -92,7 +92,7 @@
                     <li class="px-4">
                         <a href="#">
                             <div class="flex items-center gap-1.5 rounded-md px-3 py-1.5 hover:bg-zinc-100">
-                                <span class="w-4 h-4 rounded-sm bg-accent"></span>
+                                <span class="w-4 h-4 rounded-sm bg-secondary"></span>
                                 <span>Menu</span>
                             </div>
                         </a>
--- resources/views/livewire/pages/admin/layouts/topbar.blade.php
+++ resources/views/livewire/pages/admin/layouts/topbar.blade.php
@@ -1,12 +1,12 @@
 <div class="flex items-center justify-between h-8 px-4">
     <div class="flex items-center gap-1">
-        <button class="rounded-sm p-1 hover:bg-secondary" x-show="!sidebar" x-on:click="toggleSidebar">
-            <div class="w-4 h-4 bg-accent"></div>
+        <button class="group/sidebar-toggle rounded-sm p-1 hover:bg-secondary" x-show="!sidebar" x-on:click="toggleSidebar">
+            <div class="w-4 h-4 bg-secondary group-hover/sidebar-toggle:bg-accent"></div>
         </button>
     </div>
     <div class="flex items-center gap-4">
-        <button class="rounded-sm p-1 hover:bg-secondary">
-            <div class="w-4 h-4 bg-accent"></div>
+        <button class="group rounded-sm p-1 hover:bg-secondary">
+            <div class="w-4 h-4 bg-secondary group-hover:bg-accent"></div>
         </button>
     </div>
 </div>
