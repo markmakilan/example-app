41440e951447e56c75dcaa7591a49eb0d0a4de4b
Mark Makilan <mark.makilan@gets.ph>
2024-05-11 16:29:43 +0800

    ui adjustments

--- resources/views/livewire/pages/admin/layouts/topbar.blade.php
+++ resources/views/livewire/pages/admin/layouts/topbar.blade.php
@@ -1,11 +1,11 @@
 <div class="flex items-center justify-between h-8 px-4">
-    <div class="flex items-center gap-4">
-        <button class="group/sidebar-toggle rounded p-1 -mx-1 hover:bg-secondary" x-show="!sidebar" x-on:click="toggleSidebar">
+    <div class="flex items-center gap-3">
+        <button class="group/sidebar-toggle rounded p-1.5 -mx-1.5 hover:bg-secondary" x-show="!sidebar" x-on:click="toggleSidebar">
             <div class="w-4 h-4 bg-secondary rounded-sm group-hover/sidebar-toggle:bg-accent"></div>
         </button>
     </div>
-    <div class="flex items-center gap-4">
-        <button class="group rounded p-1 -mx-1 hover:bg-secondary">
+    <div class="flex items-center gap-3">
+        <button class="group rounded p-1.5 -mx-1.5 hover:bg-secondary">
             <div class="w-4 h-4 bg-secondary rounded-sm group-hover:bg-accent"></div>
         </button>
     </div>
