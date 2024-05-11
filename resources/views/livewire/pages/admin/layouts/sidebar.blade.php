cb187fdab34f98b3faa5e254372e3071e913e19c
Mark Makilan <mark.makilan@gets.ph>
2024-05-11 16:22:50 +0800

    ui enhancement

--- resources/views/livewire/pages/admin/layouts/sidebar.blade.php
+++ resources/views/livewire/pages/admin/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="w-64 h-full group/toggle-button" x-show="sidebar">
     <div class="flex flex-col h-full divide-y">
         <div class="h-12 flex items-center relative">
-            <button class="absolute hidden group/sidebar-toggle rounded p-1 -right-[12.5px] hover:bg-secondary group-hover/toggle-button:block" x-on:click="toggleSidebar">
+            <button class="absolute hidden group/sidebar-toggle rounded p-1 right-3 hover:bg-secondary group-hover/toggle-button:block" x-on:click="toggleSidebar">
                 <div class="w-4 h-4 bg-secondary rounded-sm group-hover/sidebar-toggle:bg-accent"></div>
             </button>
         </div>
