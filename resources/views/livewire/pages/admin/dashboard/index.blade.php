e7ccee1c315a14223778f7114d019a8f261c4e12
Mark Makilan <mark.makilan@gets.ph>
2024-05-13 16:12:36 +0800

    ui enhancements

--- resources/views/livewire/pages/admin/dashboard/index.blade.php
+++ resources/views/livewire/pages/admin/dashboard/index.blade.php
@@ -1,9 +1,6 @@
 <div class="flex flex-col h-full divide-y">
     <div class="min-w-full h-12 flex items-center px-4">
-        <div class="flex items-center gap-1.5">
-            <div class="w-4 h-4 rounded-sm bg-secondary"></div>
-            <h1 class="font-semibold">Dashboard</h1>
-        </div>
+        <h1 class="font-semibold">Dashboard</h1>
     </div>
     <div class="flex-1 px-4 py-2 overflow-y-auto scroller"></div>
 </div>
--- resources/views/livewire/pages/admin/layouts/topbar.blade.php
+++ resources/views/livewire/pages/admin/layouts/topbar.blade.php
@@ -1,12 +1,12 @@
 <div class="flex items-center justify-between h-8 px-4">
     <div class="flex items-center gap-3">
-        <button class="group/sidebar-toggle rounded p-1.5 -mx-1.5 hover:bg-secondary" x-show="!sidebar" x-on:click="toggleSidebar">
-            <div class="w-4 h-4 bg-secondary rounded-sm group-hover/sidebar-toggle:bg-accent"></div>
+        <button class="rounded px-1.5 py-0.5 -mx-1.5 hover:bg-secondary" x-show="!sidebar" x-on:click="toggleSidebar">
+            <p class="text-xs">Menu</p>
         </button>
     </div>
     <div class="flex items-center gap-3">
         <button class="group rounded p-1.5 -mx-1.5 hover:bg-secondary">
-            <div class="w-4 h-4 bg-secondary rounded-sm group-hover:bg-accent"></div>
+            <div class="w-4 h-4 bg-secondary rounded-full group-hover:bg-accent"></div>
         </button>
     </div>
 </div>
