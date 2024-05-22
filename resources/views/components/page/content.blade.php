e880b2c556ee83b34515d3aed4c3b099edbaf61c
Mark Makilan <mark.makilan@gets.ph>
2024-05-22 14:33:40 +0800

    use component layouting

---
+++ resources/views/components/page/content.blade.php
@@ -0,0 +1,8 @@
+<div class="flex flex-col h-full divide-y">
+    <div class="flex h-12 min-w-full items-center px-4">
+        {{ $head }}
+    </div>
+    <div class="scroller flex-1 overflow-y-auto px-4 py-2">
+        {{ $body }}
+    </div>
+</div>
--- resources/views/livewire/pages/admin/dashboard/index.blade.php
+++ resources/views/livewire/pages/admin/dashboard/index.blade.php
@@ -1,6 +1,9 @@
-<div class="flex flex-col h-full divide-y">
-    <div class="min-w-full h-12 flex items-center px-4">
+<x-page.content>
+    <x-slot name="head">
         <h1 class="font-semibold">Dashboard</h1>
-    </div>
-    <div class="flex-1 px-4 py-2 overflow-y-auto scroller"></div>
-</div>
+    </x-slot>
+    
+    <x-slot name="body">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, iure.</p>
+    </x-slot>
+</x-page.content>
