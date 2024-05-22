59046e082821ecf0fa03c3b32aa612f25f7f4d34
Mark Makilan <mark.makilan@gets.ph>
2024-05-22 16:15:36 +0800

    change from alpine to blade

--- resources/views/livewire/layouts/admin/sidebar/menu.blade.php
+++ resources/views/livewire/layouts/admin/sidebar/menu.blade.php
@@ -1,10 +1,8 @@
-<nav class="flex flex-col h-full text-sm" x-data="{ current_route_name: @entangle('current_route_name') }">
+<nav class="flex flex-col h-full text-sm">
     <ul class="flex-1 space-y-1 my-2">
         <li class="px-4">
             <a href="/admin/dashboard" wire:navigate>
-                <div 
-                    class="flex items-center gap-1.5 rounded px-3 py-1.5 hover:bg-zinc-100"
-                    x-bind:class="{'bg-zinc-100' : current_route_name == 'admin.dashboard'}">
+                <div @class(['flex items-center gap-1.5 rounded px-3 py-1.5 hover:bg-zinc-100', 'bg-zinc-100' => $current_route_name == 'admin.dashboard'])>
                     <span class="w-4 h-4 rounded-sm bg-secondary"></span>
                     <span>Dashboard</span>
                 </div>
@@ -12,9 +10,7 @@
         </li>
         <li class="px-4">
             <a href="/admin/user" wire:navigate>
-                <div 
-                    class="flex items-center gap-1.5 rounded px-3 py-1.5 hover:bg-zinc-100"
-                    x-bind:class="{'bg-zinc-100' : current_route_name == 'admin.user'}">
+                <div @class(['flex items-center gap-1.5 rounded px-3 py-1.5 hover:bg-zinc-100', 'bg-zinc-100' => $current_route_name == 'admin.user'])>
                     <span class="w-4 h-4 rounded-sm bg-secondary"></span>
                     <span>Users</span>
                 </div>
